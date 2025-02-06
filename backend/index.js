c8de3a82488a6bbd7771a12309f0c98e9ded48a3
duyaan <mahdi.ksila@medtech.tn>
2025-02-06 15:19:44 +0100

    mobile

--- backend/index.js
+++ backend/index.js
@@ -105,5 +105,5 @@ app.delete("/file/:filename", async (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
